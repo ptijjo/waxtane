7cad03e1d090777e0db5f2f8ffaae4326d38dfc3
Joseph Bangoura <ptijjo@hotmail.com>
2023-12-30 21:36:21 +0100

    client

---
+++ client/src/router/index.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Connection from "../pages/Connection";
+import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Connection/>} />
+                <Route path="/home" element={<Home/>} />
+                <Route path="/" element={ ""} />
+                <Route path="/" element={ ""} />
+                <Route path="/" element={ ""} />
+                <Route path="*" element={<ErrorPage/>} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
+
+
